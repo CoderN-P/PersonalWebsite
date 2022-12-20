098373f3642368b86eb4b942384a8d3d6680ba39
CoderN-P <neel.parpia@gmail.com>
2022-12-19 18:23:53 -0800

    Delete unecessary code and files

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-@app.route('/projects')
-def projects():
-    return render_template('projects.html')
 
 @app.route('/static/<path:path>')
 def send_static(path):
-
     return send_from_directory('static', path)
+
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=8080)
